6aa484a282f6cffa2c083da0609ffc83f1eb662b
Soumyadeep20 <soumyadeeppodder20@gmail.com>
2024-01-18 19:13:36 +0530

    Done1

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -4,14 +4,10 @@ public class DeluxePizza extends Pizza {
 
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
-        if(isVeg) {
-            price = 300+70+80;
-            bill="Base Price Of The Pizza: 450";
+        addExtraCheese();
+        addExtraToppings();
         }
-        else {
-            price = 400+120+80;
-            bill="Base Price Of The Pizza: 600";
-        }
+
         // your code goes here
     }
 }
