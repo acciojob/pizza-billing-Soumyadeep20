767d5dd12155f16694cfcac5b9f89a51695ea4f7
Soumyadeep20 <soumyadeeppodder20@gmail.com>
2024-01-18 19:03:11 +0530

    Done

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -4,6 +4,14 @@ public class DeluxePizza extends Pizza {
 
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
+        if(isVeg) {
+            price = 300+70+80;
+            bill="Base Price Of The Pizza: 450";
+        }
+        else {
+            price = 400+120+80;
+            bill="Base Price Of The Pizza: 600";
+        }
         // your code goes here
     }
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -2,33 +2,57 @@ package com.driver;
 
 public class Pizza {
 
-    private int price;
-    private Boolean isVeg;
-    private String bill;
-
+    int price;
+   public Boolean isVeg;
+    public String bill;
+/*Base Price Of The Pizza: 400
+Extra Cheese Added: 80
+Extra Toppings Added: 120
+Total Price: 600*/
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
+        if(isVeg) {
+            price = 300;
+            bill="Base Price Of The Pizza: 300";
+        }
+        else {
+            price = 400;
+            bill="Base Price Of The Pizza: 400";
+        }
         // your code goes here
     }
-
     public int getPrice(){
         return this.price;
     }
 
     public void addExtraCheese(){
+        price +=80;
+        bill +="\nExtra Cheese Added: 80";
         // your code goes here
     }
 
     public void addExtraToppings(){
+        if(isVeg) {
+            price += 70;
+            bill+="\nToppings Added: 70";
+        }
+        else {
+            price += 120;
+            bill+="\nToppings Added: 120";
+        }
+
         // your code goes here
     }
 
     public void addTakeaway(){
+        price +=20;
+        bill +="\nPaperbag Added: 20";
         // your code goes here
     }
 
     public String getBill(){
         // your code goes here
+        bill+="\nTotal Price: "+price;
         return this.bill;
     }
 }
